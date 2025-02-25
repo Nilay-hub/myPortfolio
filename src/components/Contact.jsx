cadeaa25e0481080eaabd34c89c480337c619d23
Nilay Patel <nilay.np2010@gmail.com>
2025-02-25 08:34:33 -0500

    Auto update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Lahari",
+          to_name: "Nilay",
           from_email: form.email,
-          to_email: "lakshmi333lahari@gmail.com",
+          to_email: "nilaynnp@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
